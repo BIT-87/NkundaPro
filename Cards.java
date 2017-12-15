f3576face3dfffef4159cf22320dd4b31bc6419a
BIT-87 <bitmessagebox@gmail.com>
2017-12-15 04:13:41 +0200

    JustCode
    Some basic code for learning java

---
+++ Cards.java
@@ -0,0 +1,17 @@
+package MainPackage;
+
+public class Cards{
+	private final int rank;
+	private final int suit;
+	public static final String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
+	public static final String[] RANKS = {null, "Ace", "2", "3", "4", "5", "6","7", "8", "9", "10", "Jack", "Queen", "King"};
+
+	public Cards(int rank, int suit){
+		this.rank = rank;
+		this.suit = suit;
+	}
+
+	public String toString(){
+		return RANKS[this.rank] + " of " + SUITS[this.suit];
+	}
+}
